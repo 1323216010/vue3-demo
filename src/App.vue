64f82c95f9b2cfbd427720c3aa460026cea60068
13232160100 <ypc0100@qq.com>
2023-02-04 00:22:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import arr from './assets/json/唐诗三百首.json'
+
+function search(a, x) {
+    let list = [];
+    for(let i = 0; i < a.length; i++) {
+        if(a[i].author === x) {
+            list.push(a[i]);
+        }
+    }
+    return list;
+}
+
+var poets = search(arr,'李白');
+console.log(poets);
+</script>
+
+<template>
+
+  <div id="d8">
+    <div v-for="po in poets" :key="po.id">
+      <p style="text-align: center;">{{ po.title }}</p>
+      <p style="text-align: center;">{{ po.author }}</p>
+      <p style="text-align: center;" v-for="a in po.paragraphs" :key="a">{{ a }}</p>
+      <br>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
